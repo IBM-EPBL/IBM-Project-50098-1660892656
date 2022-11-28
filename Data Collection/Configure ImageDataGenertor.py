8717331803cbbd6acff8be4e1ec0b96b2d594d89
Abineshan <abineshan100@gmail.com>
2022-11-28 12:24:08 +0530

    changes added

--- Data Collection/Configure ImageDataGenertor.py
+++
@@ -1,5 +0,0 @@
-train_datagen = ImageDataGenerator(rescale=1./255,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
-                                   horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
